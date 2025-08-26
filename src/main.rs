3225282cf1627d625de40301ea8a85d636f6de46
Joel Jensen <joelsemail28@gmail.com>
2025-08-25 20:00:00 -0600

    Implemented decaying learning rate

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod loss_functions;
 mod datasets;
 mod optimizers;
 
-use std::cmp::max;
+use std::{backtrace, cmp::max};
 
 use ndarray_linalg::krylov::R;
 use rand_distr::StandardNormal;
@@ -19,27 +19,28 @@ use crate::{
 fn main() {
     let (x, y) = spiral_data(100, 3);
     let batch_size = x.dim().0;
+    
     let mut dense1 = Layer::new(2, 64, batch_size);
     let mut activation1 = ReLU::new();
     let mut dense2 = Layer::new(64, 3, batch_size);
     let mut loss_activation = SoftmaxCategoricalCrossEntropy::new();
-    let optimizer = SGD::new(1.0);
+    let mut optimizer = SGD::new(1.0, 1e-3);
 
-    let mut acc = f64::MIN;
     let mut loss = f64::MAX;
+    let mut acc = f64::MIN;
 
     for _epoch in 0..10001 {
         dense1.forward(&x);
         activation1.forward(&dense1.outputs);
         dense2.forward(activation1.outputs());
         loss = loss_activation.forward_sparse(&dense2.outputs, &y);
-
         acc = accuracy(loss_activation.outputs(), &y);
 
         if _epoch % 100 == 0 {
             println!("epoch: {}", _epoch);
-            println!("accuracy: {}", &acc);
-            println!("loss: {}\n", &loss);
+            println!("acc: {}", acc);
+            println!("loss: {}", loss);
+            println!("lr: {}", optimizer.current_learning_rate);
         }
 
         loss_activation.backward_sparse(&loss_activation.outputs().clone(), &y);
@@ -47,10 +48,9 @@ fn main() {
         activation1.backward(dense2.dinputs());
         dense1.backward(activation1.dinputs());
 
+        optimizer.pre_update_params();
         optimizer.update_params(&mut dense1);
         optimizer.update_params(&mut dense2);
+        optimizer.post_update_paramts();
     }
-    println!("final accuracy: {}", acc);
-    println!("final loss: {}", loss);
-
 }
--- src/optimizers.rs
+++ src/optimizers.rs
@@ -1,16 +1,36 @@
 use crate::layer::Layer;
 
 pub struct SGD {
-    pub learning_rate: f64
+    pub learning_rate: f64,
+    pub decay: f64,
+    pub current_learning_rate: f64,
+    pub iterations: i64
 }
 
 impl SGD {
-    pub fn new(learning_rate: f64) -> Self {
-        SGD{ learning_rate: learning_rate }
+    pub fn new(learning_rate: f64, decay: f64) -> Self {
+        SGD{ 
+            learning_rate: learning_rate,
+            decay: decay,
+            current_learning_rate: learning_rate,
+            iterations: 0
+        }
+    }
+
+    pub fn pre_update_params(&mut self) {
+        if self.decay != 0.0 {
+            self.current_learning_rate = self.learning_rate * (
+                1.0 / (1.0 + self.decay * (self.iterations as f64))
+            );
+        }
     }
 
     pub fn update_params(&self, layer: &mut Layer) {
-        layer.weights.scaled_add(-self.learning_rate, &layer.dweights().clone());
-        layer.biases.scaled_add(-self.learning_rate, &layer.dbiases().clone());
+        layer.weights.scaled_add(-self.current_learning_rate, &layer.dweights().clone());
+        layer.biases.scaled_add(-self.current_learning_rate, &layer.dbiases().clone());
+    }
+
+    pub fn post_update_paramts(&mut self) {
+        self.iterations += 1;
     }
 }
